9d2aece1bdb27e5c1bf481021c9186a630e8cdde
aaurandt <aurandt@iastate.edu>
2023-06-02 13:50:14 -0400

    Add more parsing unit tests

--- com.rockwellcollins.atc.resolute.tests/src/com/rockwellcollins/atc/resolute/tests/UtilityFunctions.java
+++ com.rockwellcollins.atc.resolute.tests/src/com/rockwellcollins/atc/resolute/tests/UtilityFunctions.java
@@ -1,5 +1,13 @@
 package com.rockwellcollins.atc.resolute.tests;
 
+import org.eclipse.xtext.diagnostics.Severity;
+import org.eclipse.xtext.validation.Issue;
+
+import com.rockwellcollins.atc.resolute.resolute.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
@@ -10,13 +18,14 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 public class UtilityFunctions {
 	
 	/**
-	 * Takes an EMF EObject and finds its names by locating the string of the name property
+	 * Takes an EMF EObject, finds its property, and returns the property's string value if it exists
 	 *
-	 * @param eObject the EObject to find the name of
-	 * @return the name of the eObject
+	 * @param eObject the EObject to find the property of
+	 * @parm property the property's name
+	 * @return the name of the property
 	 */
-	public static String getName(EObject eObject) {
-		EStructuralFeature nameFeature = eObject.eClass().getEStructuralFeature("name");
+	public static String getStringProperty(EObject eObject, String property) {
+		EStructuralFeature nameFeature = eObject.eClass().getEStructuralFeature(property);
 		if(nameFeature instanceof EAttribute && !nameFeature.isMany()) {
 			EAttribute nameAttribute = (EAttribute)nameFeature;
 			Object nameValue = eObject.eGet(nameFeature);
@@ -26,17 +35,46 @@ public class UtilityFunctions {
 	}
 	
 	/**
+	 * Takes an EMF EObject and returns the subclass given by class_name if it exists
+	 *
+	 * @param eObject the EObject to find the subclass in
+	 * @param class_name the name of the subclass
+	 * @return the subclass
+	 */
+	private static EObject getSubclassBasedOnClassName(EObject eObject, String class_name){
+		EStructuralFeature feature = eObject.eClass().getEStructuralFeature(class_name);
+		if(feature instanceof EReference && !feature.isMany()) {
+			return (EObject)eObject.eGet(feature);
+		}		
+		return null;
+	}
+	
+	/**
+	 * Takes an EMF EObject and a property name and returns the 
+	 * class object if it exists
+	 *
+	 * @param eObject the EObject to find the property in
+	 * @param name the name of the property
+	 * @return the property titled "property_name"
+	 */
+	private static EObject getSubclassBasedOnPropertyName(EObject eObject, String property_name){
+		EList<EObject> contents = eObject.eContents();
+		for(EObject e : contents) {
+			if(getStringProperty(e, "name").equals(property_name)) {
+				return e;
+			}
+		}
+		return null;
+	}
+	
+	/**
 	 * Takes an EMF EObject and returns the ownedPublicSection if it exists
 	 *
 	 * @param eObject the EObject to find the ownedPublicSection in
 	 * @return the ownedPublicSection
 	 */
 	public static EObject getownedPublicSection(EObject eObject){
-		EStructuralFeature pub_sec_feature = eObject.eClass().getEStructuralFeature("ownedPublicSection");
-		if(pub_sec_feature instanceof EReference && !pub_sec_feature.isMany()) {
-			return (EObject)eObject.eGet(pub_sec_feature);
-		}		
-		return null;
+		return getSubclassBasedOnClassName(eObject, "ownedPublicSection");
 	}
 	
 	/**
@@ -48,13 +86,7 @@ public class UtilityFunctions {
 	 * @return the ownedClassifier titled "name"
 	 */
 	public static EObject getownedClassifier(EObject eObject, String name){
-		EList<EObject> contents = eObject.eContents();
-		for(EObject e : contents) {
-			if(getName(e).equals(name)) {
-				return e;
-			}
-		}
-		return null;
+		return getSubclassBasedOnPropertyName(eObject, name);
 	}
 	
 	/**
@@ -65,10 +97,67 @@ public class UtilityFunctions {
 	 * @param name the name of the classifier
 	 * @return the ownedClassifier titled "name"
 	 */
-	public static EObject getAnnex(EObject eObject, String name){
+	public static EObject getAnnexLibrary(EObject eObject, String name){
+		return getSubclassBasedOnPropertyName(eObject, name);
+	}
+	
+	/**
+	 * Takes an EMF EObject and an annex name and returns the 
+	 * class object if it exists
+	 *
+	 * @param eObject the EObject to find the annex in
+	 * @param name the name of the annex
+	 * @return the annex subclause titled "name"
+	 */
+	public static EObject getAnnexSubclause(EObject eObject, String name){
+		return getSubclassBasedOnPropertyName(eObject, name);
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the parsedAnnexLibrary if it exists
+	 *
+	 * @param eObject the EObject to find the parsedAnnexLibrary in
+	 * @return the parsedAnnexLibrary
+	 */
+	public static EObject getParsedAnnexLibrary(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "parsedAnnexLibrary");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the parsedAnnexSubclause if it exists
+	 *
+	 * @param eObject the EObject to find the parsedAnnexSubclause in
+	 * @return the parsedAnnexSubclause
+	 */
+	public static EObject getParsedAnnexSubclause(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "parsedAnnexSubclause");
+	}
+	
+	/**
+	 * Takes an EMF EObject and a definition name and returns the 
+	 * class object if it exists
+	 *
+	 * @param eObject the EObject to find the annex in
+	 * @param name the name of the definition
+	 * @return the definition titled "name"
+	 */
+	public static EObject getDefinition(EObject eObject, String name){
+		return getSubclassBasedOnPropertyName(eObject, name);
+	}
+	
+	/**
+	 * Takes an EMF EObject and a analysis name and returns the 
+	 * class object if it exists
+	 *
+	 * @param eObject the EObject to find the analysis in
+	 * @param name the argument of the analysis (e.g., "argue", or "prove Req1(this))")
+	 * @return the analysis with the argument "name"
+	 */
+	public static EObject getAnalysis(EObject eObject, String name){
+		name = name.substring(0,5);
 		EList<EObject> contents = eObject.eContents();
 		for(EObject e : contents) {
-			if(getName(e).equals(name)) {
+			if(getStringProperty(e, "tag").equals(name)) {
 				return e;
 			}
 		}
@@ -76,16 +165,200 @@ public class UtilityFunctions {
 	}
 	
 	/**
-	 * Takes an EMF EObject and returns the parsedAnnexSubclause
+	 * Takes an EMF EObject and returns the expr if it exists
 	 *
-	 * @param eObject the EObject to find the parsedAnnexSubclause in
-	 * @return the parsedAnnexSubclause
+	 * @param eObject the EObject to find the expr in
+	 * @return the expr
 	 */
-	public static EObject getParsedAnnex(EObject eObject){
-		EStructuralFeature parsed_annex_feature = eObject.eClass().getEStructuralFeature("parsedAnnexSubclause");
-		if(parsed_annex_feature instanceof EReference && !parsed_annex_feature.isMany()) {
-			return (EObject)eObject.eGet(parsed_annex_feature);
-		}		
+	public static EObject getExpr(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "expr");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the fn if it exists
+	 *
+	 * @param eObject the EObject to find the fn in
+	 * @return the fn
+	 */
+	public static EObject getFn(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "fn");
+	}
+	
+	/**
+	 * Takes an EMF EObject(parsed annex) and returns the fn based on the claim if it exists
+	 *
+	 * @param parsed_annex the EObject(parsed annex) to find the fn in
+	 * @param claim the string of the argument claim (e.g., "argue Req1(this)" or "prove Req2(this))")
+	 * @return the fn
+	 */
+	public static EObject getFn(EObject parsed_annex, String claim){
+		String[] str = claim.split(" ");
+		if(str.length != 2)
+			return null;
+		String argue_statement = str[0];
+		String argument_name = str[1];
+		argument_name = argument_name.substring(0, argument_name.indexOf("("));
+		EList<EObject> contents = parsed_annex.eContents();
+		for(EObject e : contents) {
+			if(getStringProperty(e, "tag").equals(argue_statement)) {
+				EObject temp_fn = getFn(getExpr(e));
+				if(getStringProperty(temp_fn, "name").equals(argument_name))
+					return temp_fn;
+			}
+		}
 		return null;
+	}
+	
+	/**
+	 * Takes an EMF EObject and an argument index and returns the 
+	 * class object if it exists
+	 *
+	 * @param eObject the EObject to find the arg in
+	 * @param index the index of the arg
+	 * @return the arg at index
+	 */
+	public static EObject getArg(EObject eObject, int index){
+		List<EObject> objects = eObject.eContents();
+		List<EObject> args = new ArrayList<EObject>();
+		for(EObject e : objects) {
+			EStructuralFeature container_feature = e.eContainingFeature();
+			if(getStringProperty((EObject)container_feature, "name").equals("args")){
+				args.add(e);
+			}
+		}
+		if(index < args.size())
+			return args.get(index);
+		else
+			return null;
+	}
+	
+	/**
+	 * Takes an EMF Object and returns the type if it exists
+	 *
+	 * @param eObject the argument to find the type in
+	 * @return the type
+	 */
+	public static EObject getType(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "type");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the body if it exists
+	 *
+	 * @param eObject the EObject to find the body in
+	 * @return the body
+	 */
+	public static EObject getClaimBody(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "body");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the binding if it exists
+	 *
+	 * @param eObject the EObject to find the binding in
+	 * @return the binding
+	 */
+	public static EObject getBinding(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "binding");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the val if it exists
+	 *
+	 * @param eObject the EObject to find the val in
+	 * @return the val
+	 */
+	public static EObject getVal(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "val");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the left property if it exists
+	 *
+	 * @param eObject the EObject to find the left property in
+	 * @return the left property
+	 */
+	public static EObject getLeft(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "left");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the right property if it exists
+	 *
+	 * @param eObject the EObject to find the right property in
+	 * @return the right property
+	 */
+	public static EObject getRight(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "right");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the id property if it exists
+	 *
+	 * @param eObject the EObject to find the id property in
+	 * @return the id property
+	 */
+	public static EObject getID(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "id");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the unit if it exists
+	 *
+	 * @param eObject the EObject to find the unit in
+	 * @return the unit
+	 */
+	public static EObject getUnit(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "unit");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the map if it exists
+	 *
+	 * @param eObject the EObject to find the map in
+	 * @return the map
+	 */
+	public static EObject getMap(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "map");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the cond property if it exists
+	 *
+	 * @param eObject the EObject to find the cond property in
+	 * @return the cond property
+	 */
+	public static EObject getCond(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "cond");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the then property if it exists
+	 *
+	 * @param eObject the EObject to find the then property in
+	 * @return the then property
+	 */
+	public static EObject getThen(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "then");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the else property if it exists
+	 *
+	 * @param eObject the EObject to find the else property in
+	 * @return the else property
+	 */
+	public static EObject getElse(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "else");
+	}
+	
+	/**
+	 * Takes an EMF EObject and returns the else property if it exists
+	 *
+	 * @param eObject the EObject to find the else property in
+	 * @return the else property
+	 */
+	public static EObject getFilter(EObject eObject){
+		return getSubclassBasedOnClassName(eObject, "filter");
 	}
 }
